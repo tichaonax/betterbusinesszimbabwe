2adf63786aa18685c41c1e9c7f59c32e0acb9b7e
Tichaona Hwandaza <tichaona@yahoo.com>
2017-01-13 10:46:15 -0600

    Add test for List component

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,14 +5,32 @@ var $ = require('jquery');
 var TestUtils = require('react-addons-test-utils');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
 
-describe('TodoList',()=>{
-    it('should exist', ()=>{
+describe('TodoList', () => {
+    it('should exist', () => {
         expect(TodoList).toExist();
     });
 
+    it('should render one Todo component for each todo item', () => {
+        var todos = [
+            {
+                id: 1,
+                text: 'Do something'
+            }, {
+                id: 2,
+                text: 'Water the plants'
+            }];
+
+        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+
+        var todoComponent = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+        expect(todoComponent.length).toBe(todos.length);
 
 
+
+    });
 
 
 });
