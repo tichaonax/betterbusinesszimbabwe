adf1317f3fe8fd19ff22fae46891e85a171305fd
Tichaona Hwandaza <tichaona@yahoo.com>
2017-01-14 11:59:45 -0600

    added test to verify onToggleItem is called with correct id

--- app/tests/components/TodoItem.test.jsx
+++ app/tests/components/TodoItem.test.jsx
@@ -6,13 +6,30 @@ var TestUtils = require('react-addons-test-utils');
 
 var TodoItem = require('TodoItem');
 
-describe('TodoItem',()=>{
-    it('should exist', ()=>{
+describe('TodoItem', () => {
+    it('should exist', () => {
         expect(TodoItem).toExist();
     });
 
+    it('should call onToggleItem prop function with correct id on click', () => {
+        var todoItem = {
+            id: 23,
+            text: 'Toggle test feature',
+            completed: false
+        };
+
+        var spy = expect.createSpy();
+        var todoItem = TestUtils.renderIntoDocument(<TodoItem {...todoItem} onToggleItem={spy}/>);
+
+        var $el = $(ReactDOM.findDOMNode(todoItem));
+
+        //the div is the first component
+        TestUtils.Simulate.click($el[0]);
+
+        expect(spy).toHaveBeenCalledWith(23);
 
 
+    });
 
 
 });
