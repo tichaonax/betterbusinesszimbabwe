a0d8792501e3b82cb5f68fdfc35d5646660e8f56
Tichaona Hwandaza <tichaona@yahoo.com>
2017-01-14 11:43:07 -0600

    added validation tests for completed status

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -6,12 +6,12 @@ var TestUtils = require('react-addons-test-utils');
 
 var TodoApp = require('TodoApp');
 
-describe('TodoApp',()=>{
-    it('should exist', ()=>{
+describe('TodoApp', () => {
+    it('should exist', () => {
         expect(TodoApp).toExist();
     });
 
-    it('shoould add todo item to the todos state on handleAddTodoItem',()=>{
+    it('shoould add todo item to the todoItemss state on handleAddTodoItem', () => {
         var todoText = 'test text';
         var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
 
@@ -24,10 +24,37 @@ describe('TodoApp',()=>{
 
         expect(todoApp.state.todoItems[0].text).toBe(todoText);
 
-    })
+    });
+
+
+    it('should toggle completed value when handleToggleItem is called', () => {
+        var todoItem = {
+            id: 23,
+            text: 'Toggle test feature',
+            completed: false
+        };
+
+        var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+        // set initial only array item
+        todoApp.setState({
+            todoItems: [todoItem]
+        });
+
+        //validate the todoItem has completed status of false
+
+        expect(todoApp.state.todoItems[0].completed).toBe(false);
+
+        //call handleToggleItem with id 23 to toggle completed status
+        todoApp.handleToggleItem(23);
+
+        //validate that the completed status changed
+        expect(todoApp.state.todoItems[0].completed).toBe(true);
 
 
 
+
+    });
 
 
 });
